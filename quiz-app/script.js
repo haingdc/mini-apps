cffaa81374e52558dfa6d854e3fe05548d169502
hai.nguyen <>
2020-11-20 11:52:34 +0700

    function the quiz

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -32,19 +32,24 @@ var quizData = [
     correct: 'd',
   },
 ];
+
+var quiz = document.getElementById('quiz');
 var questionEl = document.getElementById('question');
 var a_text = document.getElementById('a_text');
 var b_text = document.getElementById('b_text');
 var c_text = document.getElementById('c_text');
 var d_text = document.getElementById('d_text');
 var submitBtn = document.getElementById('submit');
+var answerEls = document.querySelectorAll('.answer');
 
 var currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 
 function loadQuiz() {
+  deselectAnswer();
   var currentQuizData = quizData[currentQuiz];
   questionEl.innerHTML = currentQuizData.question;
   a_text.innerText = currentQuizData.a;
@@ -54,11 +59,35 @@ function loadQuiz() {
 }
 
 submitBtn.addEventListener('click', function todo() {
-  currentQuiz++;
-  if (currentQuiz < quizData.length) {
-    loadQuiz();
-  } else {
-    // todo: show result
-    alert('You finished! Get yourself an Lemonade')
+  var answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+        <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+        <button onclick="location.reload()">Reload</button>
+      `;
+    }
   }
 });
+
+function getSelected() {
+  let answer = undefined;
+  answerEls.forEach(answerEl => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+
+function deselectAnswer() {
+  answerEls.forEach(answerEl => {
+    answerEl.checked = false
+  });
+}
