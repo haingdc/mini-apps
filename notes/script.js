a89b48887c01f92bb75eb7720de5e2cfe8923cd9
hai.nguyen <haingdc@gmail.com>
2020-11-25 08:38:14 +0700

    show markdown preview at first

--- notes/script.js
+++ notes/script.js
@@ -19,8 +19,8 @@ function addNewNote(text = '') {
       <button class="edit"> <i class="fas fa-edit"></i> </button>
       <button class="delete"> <i class="fas fa-trash-alt"></i> </button>
     </div>
-    <div class="main hidden"></div>
-    <textarea></textarea>
+    <div class="main ${text ? '' : 'hidden'}"></div>
+    <textarea class="${text ? 'hidden' : ''}"></textarea>
   `;
   document.body.appendChild(note);
 
