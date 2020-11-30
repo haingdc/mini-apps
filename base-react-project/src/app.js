72b6675fb625055c6e8b242900154bcac76f5243
hai.nguyen <haingdc@gmail.com>
2020-12-01 01:39:49 +0700

    init base-react-project

---
+++ base-react-project/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Jumbotron from './components/jumbotron';
+import jumboData from './fixtures/jumbo.json';
+
+function App() {
+  return (
+    <p>Hello angel</p>
+  );
+}
+
+export default App;
