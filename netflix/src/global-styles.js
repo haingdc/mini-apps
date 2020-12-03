06c6c238852942ad90b61d177c85a075230a12e2
hai.nguyen <haingdc@gmail.com>
2020-12-04 01:01:57 +0700

    add cursor effect

--- netflix/src/global-styles.js
+++ netflix/src/global-styles.js
@@ -9,4 +9,36 @@ export var GlobalStyles = createGlobalStyle`
     color: #333;
     font-size: 16px;
   }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    --cursor-stroke: #fff;
+    --cursor-fill: none;
+    --cursor-stroke-width: 1px;
+  }
+
+  .cursor {
+    display: none;
+  }
+
+  @media (any-pointer: fine) {
+    .cursor {
+      position: fixed;
+      top: 0;
+      left: 0;
+      display: block;
+      pointer-events: none;
+      z-index: 9999;
+    }
+    .cursor__inner {
+      fill: var(--cursor-fill);
+      stroke: var(--cursor-stroke);
+      stroke-width: var(--cursor-stroke-width);
+      opacity: 1;
+    }
+    .no-js .cursor {
+      display: none;
+    }
+  }
 `;
--- netflix/src/index.js
+++ netflix/src/index.js
@@ -3,11 +3,23 @@ import { render } from 'react-dom';
 import App from './app';
 import 'normalize.css';
 import { GlobalStyles } from './global-styles';
+import Cursor from './utils/cursor';
 
 render(
   <>
     <GlobalStyles />
     <App />
+    <svg class="cursor" width="30" height="30" viewBox="0 0 30 30">
+      <circle class="cursor__inner" cx="15" cy="15" r="7.5" />
+    </svg>
   </>,
   document.getElementById('root')
 );
+
+// Initialize custom cursor
+const cursor = new Cursor(document.querySelector('.cursor'));
+// Mouse effects on all links and buttons
+[...document.querySelectorAll('a, .gallery__item-more, .back')].forEach(link => {
+  link.addEventListener('mouseenter', () => cursor.enter());
+  link.addEventListener('mouseleave', () => cursor.leave());
+});
