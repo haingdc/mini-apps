fb295c34efe3206db1158c21aa74252f3e5dc70e
hai.nguyen <haingdc@gmail.com>
2020-12-08 01:07:54 +0700

    add basic movement

---
+++ basic-animations/ball/index.js
@@ -0,0 +1,13 @@
+function moveBall() {
+  var ballObj = document.getElementById("ball");
+
+  ballObj.style.left = "70px";
+  ballObj.style.top = "80px";
+}
+
+function reset() {
+  var ballObj = document.getElementById("ball");
+  ballObj.style.left = "50px";
+  ballObj.style.top = "30px";
+}
+
