56b0951b2bec9ac80b19e1e9a989119ee1e2f1f7
hai.nguyen <haingdc@gmail.com>
2020-12-08 11:14:43 +0700

    parabolic move

--- basic-animations/ball/index.js
+++ basic-animations/ball/index.js
@@ -1,8 +1,5 @@
-function moveBall() {
-  var ballObj = document.getElementById("ball");
-
-  ballObj.style.left = "70px";
-  ballObj.style.top = "80px";
+function start() {
+  startBall2();
 }
 
 function reset() {
@@ -11,3 +8,28 @@ function reset() {
   ballObj.style.top = "30px";
 }
 
+function moveDomObj(id, left, top) {
+  var domObj = document.getElementById(id);
+  domObj.style.left = left + 'px';
+  domObj.style.top  = top  + 'px';
+}
+
+var timer2 = undefined;
+var s = 0;
+
+function stepBall2() {
+  var x = 50 + 4 * s;
+  var y = 30 + 1.6 * s * s; // a parabolic path y=x*x
+  console.log({x,y})
+  moveDomObj('ball', x, y);
+  s++;
+  if (s < 14) {
+    timer2 = setTimeout(stepBall2, 50);
+  } else {
+    s = 0;
+  }
+}
+
+function startBall2() {
+  timer2 = setTimeout(stepBall2, 100);
+}
