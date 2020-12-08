da550fa5a545c4a562a232060b99ba6355b0a907
hai.nguyen <haingdc@gmail.com>
2020-12-08 17:15:50 +0700

    filtering

--- netflix/src/pages/browse.js
+++ netflix/src/pages/browse.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
   var { series } = useContent('series');
   var { films } = useContent('films');
-  console.log({series, films});
+  var slides = selectionFilter({ series, films });
+  console.log(slides);
+
   return <p>Browse</p>
 }
