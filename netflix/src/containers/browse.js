09bfafe2298028a53139340cb0952320db418ee8
hai.nguyen <haingdc@gmail.com>
2020-12-10 17:09:42 +0700

    add logo

--- netflix/src/containers/browse.js
+++ netflix/src/containers/browse.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
 import { Header, Loading } from '../components';
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
 
 export function BrowseContainer({ slides }) {
   var [profile, setProfile] = useState({});
@@ -22,8 +24,11 @@ export function BrowseContainer({ slides }) {
         <Loading.ReleaseBody />
       )}
       <Header src="joker1">
+        <Header.Frame>
+          <Header.Logo to={ROUTES.HOME} alt="Netflix" src={logo} />
+        </Header.Frame>
         <Header.Feature>
-        <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
           <Header.Text>Joker is a 2019 American psychological thriller film directed and produced by Todd Phillips, who co-wrote the screenplay with Scott Silver. The film, based on DC Comics characters, stars Joaquin Phoenix as the Joker and provides an alternative origin story for the character. Set in 1981, it follows Arthur Fleck, a failed clown and stand-up comedian whose descent into insanity and nihilism inspires a violent counter-cultural revolution against the wealthy in a decaying Gotham City. Robert De Niro, Zazie Beetz, Frances Conroy, Brett Cullen, Glenn Fleshler, Bill Camp, Shea Whigham, and Marc Maron appear in supporting roles. Joker was produced by Warner Bros. Pictures, DC Films, and Joint Effort, in association with Bron Creative and Village Roadshow Pictures, and distributed by Warner Bros.</Header.Text>
         </Header.Feature>
       </Header>
