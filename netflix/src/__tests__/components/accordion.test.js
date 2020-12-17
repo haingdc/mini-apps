80c1f374c5591f099b70196b94c57d8434b10954
hai.nguyen <haingdc@gmail.com>
2020-12-17 21:59:18 +0700

    add card test

--- netflix/src/__tests__/components/accordion.test.js
+++ netflix/src/__tests__/components/accordion.test.js
@@ -25,7 +25,6 @@ describe('<Accordion />', () => {
 
     var whatIsNetflixBodyText = "Netflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices.\n\nYou can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
 
-    console.log(queryByText(whatIsNetflixBodyText))
     expect(queryByText(whatIsNetflixBodyText)).toBeFalsy();
     fireEvent.click(queryByText('What is Netflix?'));
     expect(queryByText(whatIsNetflixBodyText)).toBeTruthy();
