756ef96ec60d4c2a4f846ad242cde4bc8a3f17d6
hai.nguyen <haingdc@gmail.com>
2020-12-25 08:47:36 +0700

    fix navbar is overlap + show some elements

--- lading-pages/2-great-escape-landing-page/index.js
+++ lading-pages/2-great-escape-landing-page/index.js
@@ -11,10 +11,10 @@ function animate() {
   
   // NAVBAR
   TweenMax.fromTo(".navbar", {
-    delay: 2.5,
     opacity: 0,
     y: 20,
   }, {
+    delay: 2.5,
     duration: 1.5,
     opacity: 1,
     y: 0,
@@ -30,40 +30,40 @@ function animate() {
   }, 0.08)
   
   // TEXT
-  TweenMax.from(".text h1 .hidetext", 1.5, {
-    delay: 1.6,
-    y: "100%",
-    ease: Expo.easeInOut
-  })
-  
-  TweenMax.from(".text p .hidetext", 1.5, {
-    delay: 2,
-    y: "100%",
-    ease: Expo.easeInOut
-  })
+  // TweenMax.from(".text h1 .hidetext", 1.5, {
+  //   delay: 1.6,
+  //   y: "100%",
+  //   ease: Expo.easeInOut
+  // })
+
+  // TweenMax.from(".text p .hidetext", 1.5, {
+  //   delay: 2,
+  //   y: "100%",
+  //   ease: Expo.easeInOut
+  // })
   
   // DESC
-  TweenMax.staggerFrom(".desc ul li", 1.5, {
-    delay: 2,
-    opacity: 0,
-    y: 20,
-    ease: Expo.easeInOut
-  }, 0.1)
-  
-  TweenMax.from(".desc p", 1.5, {
-    delay: 2.3,
-    opacity: 0,
-    y: 20,
-    ease: Expo.easeInOut
-  })
+  // TweenMax.staggerFrom(".desc ul li", 1.5, {
+  //   delay: 2,
+  //   opacity: 0,
+  //   y: 20,
+  //   ease: Expo.easeInOut
+  // }, 0.1)
+
+  // TweenMax.from(".desc p", 1.5, {
+  //   delay: 2.3,
+  //   opacity: 0,
+  //   y: 20,
+  //   ease: Expo.easeInOut
+  // })
   
   // SCROLLDOWN
-  TweenMax.from(".scrolldown", 1.5, {
-    delay: 2.7,
-    opacity: 0,
-    y: 200,
-    ease: Expo.easeInOut
-  })
+  // TweenMax.from(".scrolldown", 1.5, {
+  //   delay: 2.7,
+  //   opacity: 0,
+  //   y: 200,
+  //   ease: Expo.easeInOut
+  // })
   
   // BOTTOMNAV
   TweenMax.from(".bottomnav", 1.5, {
