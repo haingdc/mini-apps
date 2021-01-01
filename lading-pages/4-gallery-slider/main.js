580ea4ef65db596d68066f5a9c4fc961d4479189
hai.nguyen <haingdc@gmail.com>
2021-01-01 09:32:15 +0700

    effect when changing slide

--- lading-pages/4-gallery-slider/main.js
+++ lading-pages/4-gallery-slider/main.js
@@ -5,17 +5,17 @@ var slider = new Swiper(".swiper-container", {
   mousewheel: true
 })
 
-// slider.on('slideChange', function () {
-//   TweenMax.to('.slide-text span', .2, {
-//     y: '-100px',
-//   })
-//   TweenMax.to('.slide-number span', .2, {
-//     x: '-100px',
-//   })
-//   TweenMax.to('.swiper-slide-active', .5, {
-//     scale: .85
-//   })
-// })
+slider.on('slideChange', function () {
+  TweenMax.to('.slide-text span', .2, {
+    y: '-100px',
+  })
+  TweenMax.to('.slide-number span', .2, {
+    x: '-100px',
+  })
+  TweenMax.to('.swiper-slide-active', .5, {
+    scale: .85
+  })
+})
 
 // slider.on('slideChangeTransitionEnd', function () {
 
