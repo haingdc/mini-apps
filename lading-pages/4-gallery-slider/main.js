83044eec5420a2f7322f2e9a4c16efbc233f7cfb
hai.nguyen <haingdc@gmail.com>
2021-01-03 21:51:41 +0700

    autoAlpha for animation opacity

--- lading-pages/4-gallery-slider/main.js
+++ lading-pages/4-gallery-slider/main.js
@@ -41,47 +41,47 @@ slider.on('slideChangeTransitionEnd', function () {
     ease: Power4.easeOut,
   })
 
-//   TweenMax.to('.swiper-slide-active .slide-text', 0, {
-//     autoAlpha: 1
-//   })
-//   TweenMax.to('.swiper-slide-active .slide-number', 0, {
-//     autoAlpha: 1
-//   })
+  TweenMax.to('.swiper-slide-active .slide-text', 0, {
+    autoAlpha: 1
+  })
+  TweenMax.to('.swiper-slide-active .slide-number', 0, {
+    autoAlpha: 1
+  })
 
-//   TweenMax.to('.swiper-slide-next .slide-text', 0, {
-//     autoAlpha: 0
-//   })
-//   TweenMax.to('.swiper-slide-prev .slide-text', 0, {
-//     autoAlpha: 0
-//   })
+  TweenMax.to('.swiper-slide-next .slide-text', 0, {
+    autoAlpha: 0
+  })
+  TweenMax.to('.swiper-slide-prev .slide-text', 0, {
+    autoAlpha: 0
+  })
 
-//   TweenMax.to('.swiper-slide-next .slide-number', 0, {
-//     autoAlpha: 0
-//   })
-//   TweenMax.to('.swiper-slide-prev .slide-number', 0, {
-//     autoAlpha: 0
-//   })
+  TweenMax.to('.swiper-slide-next .slide-number', 0, {
+    autoAlpha: 0
+  })
+  TweenMax.to('.swiper-slide-prev .slide-number', 0, {
+    autoAlpha: 0
+  })
+}); // transition end
+
+TweenMax.to('.swiper-slide-next .slide-text', 0, {
+  autoAlpha: 0
+})
+TweenMax.to('.swiper-slide-prev .slide-text', 0, {
+  autoAlpha: 0
 })
 
-// TweenMax.to('.swiper-slide-next .slide-text', 0, {
-//   autoAlpha: 0
-// })
-// TweenMax.to('.swiper-slide-prev .slide-text', 0, {
-//   autoAlpha: 0
-// })
+TweenMax.to('.swiper-slide-next .slide-number', 0, {
+  autoAlpha: 0
+})
+TweenMax.to('.swiper-slide-prev .slide-number', 0, {
+  autoAlpha: 0
+})
 
-// TweenMax.to('.swiper-slide-next .slide-number', 0, {
-//   autoAlpha: 0
-// })
-// TweenMax.to('.swiper-slide-prev .slide-number', 0, {
-//   autoAlpha: 0
-// })
+TweenMax.to('.swiper-slide', 0, {
+  scale: .85,
+})
 
-// TweenMax.to('.swiper-slide', 0, {
-//   scale: .85,
-// })
-
-// TweenMax.to('.swiper-slide-active', 0, {
-//   scale: 1,
-// })
+TweenMax.to('.swiper-slide-active', 0, {
+  scale: 1,
+})
 
