546570a4352bd0a9fcdcbcdfb108a6bc89d34387
hai.nguyen <haingdc@gmail.com>
2021-01-14 23:37:56 +0700

    animate overlay

---
+++ lading-pages/9-hip-hop/index.js
@@ -0,0 +1,18 @@
+TweenMax.to('.first', 1.5, {
+  delay: .2,
+  x: '-100%',
+  ease: Expo.easeInOut
+})
+
+TweenMax.to('.second', 1.5, {
+  delay: .4,
+  x: '-100%',
+  ease: Expo.easeInOut
+})
+
+TweenMax.to('.third', 1.5, {
+  delay: .6,
+  x: '-100%',
+  ease: Expo.easeInOut
+})
+
