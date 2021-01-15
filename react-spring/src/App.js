8488aa67f5b76c4f13defb5af6997bec3cf97f58
hai.nguyen <haingdc@gmail.com>
2021-01-15 23:33:04 +0700

    baby step to react-spring

--- react-spring/src/App.js
+++ react-spring/src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 import './App.css';
 
 export default function App() {
   const [showing, setShowing] = useState(false);
+
+  const fadeStyles = useSpring({
+    from: { opacity: 0 },
+    to: {
+      opacity: showing ? 1 : 0
+    }
+  });
+
   return (
     <div>
-      <div style={{ opacity: showing ? 1 : 0 }}>
+      <animated.div style={fadeStyles}>
         This content will fade in and fade out
-      </div>
+      </animated.div>
       <button onClick={() => setShowing(val => !val)}>Toggle</button>
       <hr />
     </div>
