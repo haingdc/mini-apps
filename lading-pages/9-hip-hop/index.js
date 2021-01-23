d371a82ad3db48248efa4b3d5086f0e0d43060f7
hai.nguyen <haingdc@gmail.com>
2021-01-23 21:28:24 +0700

    animate search and cart

--- lading-pages/9-hip-hop/index.js
+++ lading-pages/9-hip-hop/index.js
@@ -12,7 +12,20 @@ function animate() {
     x: -20,
     ease: Power4.easeInOut
   }, 0.08)
-  
+
+  TweenMax.from('.search', 1, {
+    delay: 1.6,
+    opacity: 0,
+    x: -20,
+    ease: Expo.easeInOut
+  })
+
+  TweenMax.from('.cart', 1, {
+    delay: 1.8,
+    opacity: 0,
+    x: -20,
+    ease: Expo.easeInOut
+  })
 
   TweenMax.fromTo('.first', {
     x: 0,
