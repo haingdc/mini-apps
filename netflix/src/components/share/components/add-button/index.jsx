9bcf56c31419b34e762b9082797f88d7ef2cdee5
hai.nguyen <haingdc@gmail.com>
2021-02-05 19:39:32 +0700

    toggle input

--- netflix/src/components/share/components/add-button/index.jsx
+++ netflix/src/components/share/components/add-button/index.jsx
@@ -1,35 +1,42 @@
+import { useState } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import './style.scss';
 
 export function AddButton(props) {
   const { isShowInput, onAdd, value, setValue } = props;
+  const [ isArea, setArea ] = useState(false);
   return (
     <div className="add-button-wrapper">
-      <textarea
-        rows="1"
-        autoCapitalize="none"
-        autoCorrect="off"
-        autoComplete="false"
-        spellCheck="false"
-        className="tag__input"
-        onKeyPress={(event) => {
-          var key = window.event.keyCode;
-          if (key === 13 && event.target.value !== '') {
-            event.preventDefault();
-            setValue('');
-            onAdd(event.target.value);
-          }
-        }}
-        onChange={event => setValue(event.target.value)}
-        value={value}
-      />
+      {
+        isArea ?
+          <textarea
+            rows="1"
+            autoCapitalize="none"
+            autoCorrect="off"
+            autoComplete="false"
+            spellCheck="false"
+            className="tag__input"
+            onKeyPress={(event) => {
+              var key = window.event.keyCode;
+              if (key === 13 && event.target.value !== '') {
+                event.preventDefault();
+                setValue('');
+                onAdd(event.target.value);
+              }
+            }}
+            onChange={event => setValue(event.target.value)}
+            value={value}
+          /> : undefined
+      }
       <div
         className="button--add"
         style={{
           backgroundColor: '#4d4b52d',
           width: '30px',
           height: '30px',
-        }}>
+        }}
+        onClick={() => setArea(v => !v)}
+      >
         <AiOutlinePlus  />
       </div>
     </div>
