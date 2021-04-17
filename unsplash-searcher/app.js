cf5855de111cce5ef239fce3e6467c58eff4d17c
hai.nguyen <haingdc@gmail.com>
2021-04-17 21:39:48 +0700

    add react without jsx

---
+++ unsplash-searcher/app.js
@@ -0,0 +1,59 @@
+function ListItem(props) {
+  return React.createElement('li', { className: 'list-item', onClick: props.onDelete }, props.title)
+}
+
+class List extends React.Component {
+
+  constructor() {
+    super()
+    this.state = { items: ['Apple', 'Banana'] }
+  }
+
+  addItemHandler() {
+    this.setState(prevState => {
+      return { items: prevState.items.concat('Apples') }
+    })
+  }
+
+  onDelete(text) {
+    this.setState(prevState =>
+    {
+      return {
+        items: prevState.items.filter(item => {
+          return item != text
+        })
+      }
+    })
+  }
+
+  render() {
+    return React.createElement
+    ('div', null,
+      [
+        React.createElement
+          (
+            'ul', { key: 'fruit-list' },
+              this.state.items.map
+              (
+                item => {
+                  return React.createElement( ListItem, {
+                    title : item,
+                    key: item,
+                    onDelete: this.onDelete.bind(this, item)
+                  } )
+                }
+              )
+          ),
+        React.createElement
+          (
+            'button', { key: 'fruit-button', onClick: this.addItemHandler.bind(this) }, 'Add Fruit  '
+          )
+      ]
+    )
+  }
+}
+
+ReactDOM.render(
+  React.createElement(List),
+  document.querySelector('#fruit-list')
+)
