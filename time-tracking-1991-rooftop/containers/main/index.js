3c84bde16220e9fe9c0a94a84ff50091fdf7e821
hai.nguyen <haingdc@gmail.com>
2021-05-22 12:14:53 +0700

    rough event

--- time-tracking-1991-rooftop/containers/main/index.js
+++ time-tracking-1991-rooftop/containers/main/index.js
@@ -14,6 +14,37 @@ function Main() {
 function Section1() {
   return 'Left'
 }
+
+var eventsByDay = [
+  {
+    id: 'Monday',
+    events: [
+      {
+        id: 'ATVPBWQIWR',
+        title: 'Tod 1',
+        hour: '12:15'
+      },
+    ],
+  },
+  {
+    id: 'Tuesday',
+  },
+  {
+    id: 'Wednesday',
+  },
+  {
+    id: 'Thursday',
+  },
+  {
+    id: 'Friday',
+  },
+  {
+    id: 'Saturday',
+  },
+  {
+    id: 'Sunday',
+  },
+]
 function Section2() {
   const  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
   const hours = ['1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 AM', '13 AM', '14 AM', '15 AM', '16 AM', '17 AM', '18 AM', '19 AM', '20 AM', '21 AM', '22 AM', '23 AM', '24 AM']
@@ -48,12 +79,24 @@ function Section2() {
           e('div', { key: 'lefty vertical line', className: 'vertical-line vertical-line--lefty' }),
           [
             // e('div', { key: 'gmt', className : 'vertical-line-freeze' }, 'GMT+7'),
-            days.map(function renderColumns(day) {
+            eventsByDay.map(function renderColumns(day) {
               return e(
                 'div',
-                { key: day, className: 'vertical-line' },
-                e('div', { className: 'cell--header' }, day)
-                // đặt các hình chữ nhật thông tin ở đây
+                { key: day.id, className: 'vertical-line' },
+                [
+                  e('div', { key: `${day.id}-header`, className: 'cell--header' }, day.id),
+                  // đặt các hình chữ nhật thông tin ở đây
+                  day?.events?.length ?  e('div', { key: day.events[0], style: {
+                    position: 'absolute',
+                    top: '230px',
+                    left: 0,
+                    right: 0,
+                    width: '80%',
+                    background: 'lightblue',
+                    margin: '0 auto',
+                    zIndex: 3,
+                  } }, day.events[0].title) : undefined,
+                ]
               )
             })
           ]
@@ -64,3 +107,7 @@ function Section2() {
 }
 
 export default Main
+
+function Event() {
+  return 'Event'
+}
