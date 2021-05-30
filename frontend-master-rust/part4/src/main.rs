486be52fab380f24f5599d4904ef636a3bed474f
hai.nguyen <haingdc@gmail.com>
2021-05-30 18:39:43 +0700

    add exercise

---
+++ frontend-master-rust/part4/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+  let mut city_names = vec!["Pythonia", "Javasburg", "C by the Sea", "Rustville"];
+
+  let last_city = match city_names.pop() {
+    Some(inner_value) => { inner_value }
+    None => { "" }
+  };
+
+  if last_city.starts_with("R") {
+      println!("“{}” starts with an R!", last_city);
+  } else {
+      println!("“{}” doesn't start with R", last_city);
+  }
+
+  city_names.push(last_city);
+
+  println!("Here is the full list of cities:");
+  for city_name in city_names.iter() {
+    println!("* {}", city_name);
+  }
+}
