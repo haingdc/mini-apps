c77fc9692c95da991c447d436251e1b72501eada
hai.nguyen <haingdc@gmail.com>
2021-06-03 00:02:21 +0700

    get method with dynamic query string

--- rusty-rocket-live-7140327/src/main.rs
+++ rusty-rocket-live-7140327/src/main.rs
@@ -12,6 +12,19 @@ fn hello_world() -> &'static str {
   "Hello, world! from root"
 }
 
+#[get("/<name>")]
+fn greeting(name: String) -> String {
+  format!("Hello {}", name)
+}
+
+#[get("/hello?<name>&<salutation>")]
+fn query_greeting(name : String, salutation: Option<String>) -> String {
+  match salutation {
+    Some(s) => format!("{} {}", s, name),
+    None => format!("Hello {}", name),
+  }
+}
+
 fn main() {
   // rocket::ignite().mount("/api", routes![world]).launch();
   let cfg = rocket::config::Config::build(rocket::config::Environment::Development)
@@ -23,5 +36,7 @@ fn main() {
   rocket::custom(cfg).mount("/api", routes![
     world,
     hello_world,
+    query_greeting,
+    greeting,
   ]).launch();
 }
