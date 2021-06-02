b33c3428ff015126937e7620173a174a141043be
hai.nguyen <haingdc@gmail.com>
2021-06-02 23:49:08 +0700

    use 127.0.0.1 instead of localhost

--- rusty-rocket-live-7140327/src/main.rs
+++ rusty-rocket-live-7140327/src/main.rs
@@ -4,9 +4,24 @@
 
 #[get("/world")]
 fn world() -> &'static str {
-    "Hello, world!"
+  "Hello, world!"
+}
+
+#[get("/")]
+fn hello_world() -> &'static str {
+  "Hello, world! from root"
 }
 
 fn main() {
-    rocket::ignite().mount("/api", routes![world]).launch();
+  // rocket::ignite().mount("/api", routes![world]).launch();
+  let cfg = rocket::config::Config::build(rocket::config::Environment::Development)
+        .address("127.0.0.1")
+        .port(8000)
+        .extra("template_dir",  "web/templates")
+        .unwrap();
+
+  rocket::custom(cfg).mount("/api", routes![
+    world,
+    hello_world,
+  ]).launch();
 }
