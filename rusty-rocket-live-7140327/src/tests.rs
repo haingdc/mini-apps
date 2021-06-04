3c5d66d6e9ba6b0cf254dbfd776657a6ac620356
hai.nguyen <haingdc@gmail.com>
2021-06-04 14:53:50 +0700

    add some other tests

--- rusty-rocket-live-7140327/src/tests.rs
+++ rusty-rocket-live-7140327/src/tests.rs
@@ -3,17 +3,38 @@ use rocket::local::Client;
 use rocket::http::Status;
 
 #[test]
-fn index() {
-    let client = Client::new(rocket()).expect("valid rocket instance");
-    let mut response = client.get("/api").dispatch();
-    assert_eq!(response.status(), Status::Ok);
-    assert_eq!(response.body_string(), Some("It's homepage aka index".into()));
+fn test_index() {
+  let client = Client::new(rocket()).expect("valid rocket instance");
+  let mut response = client.get("/api").dispatch();
+  assert_eq!(response.status(), Status::Ok);
+  assert_eq!(response.body_string(), Some("It's homepage aka index".into()));
 }
 
 #[test]
-fn world() {
-    let client = Client::new(rocket()).expect("valid rocket instance");
-    let mut response = client.get("/api/world").dispatch();
-    assert_eq!(response.status(), Status::Ok);
-    assert_eq!(response.body_string(), Some("Hello, world!".into()));
+fn test_world() {
+  let client = Client::new(rocket()).expect("valid rocket instance");
+  let mut response = client.get("/api/world").dispatch();
+  assert_eq!(response.status(), Status::Ok);
+  assert_eq!(response.body_string(), Some("Hello, world!".into()));
+}
+
+#[test]
+fn test_greeting() {
+  let client = Client::new(rocket()).unwrap();
+  let mut response = client.get("/api/rainer").dispatch();
+  assert_eq!(response.body_string(), Some("Hello rainer".into()));
+}
+
+#[test]
+fn test_query_greeting_with_salutation() {
+  let client = Client::new(rocket()).unwrap();
+  let mut response = client.get("/api/hello?name=rainer&salutation=Hi").dispatch();
+  assert_eq!(response.body_string(), Some("Hi rainer".into()))
+}
+
+#[test]
+fn test_query_greeting_without_salutation() {
+  let client = Client::new(rocket()).unwrap();
+  let mut response = client.get("/api/hello?name=rainer").dispatch();
+  assert_eq!(response.body_string(), Some("Hello rainer".into()))
 }
