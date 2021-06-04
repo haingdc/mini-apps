e0ecf8885f0804cc421fc4e72c7ddb82a1b1d47e
hai.nguyen <haingdc@gmail.com>
2021-06-04 13:04:59 +0700

    test world

--- rusty-rocket-live-7140327/src/tests.rs
+++ rusty-rocket-live-7140327/src/tests.rs
@@ -3,9 +3,17 @@ use rocket::local::Client;
 use rocket::http::Status;
 
 #[test]
-fn hello_world() {
+fn index() {
     let client = Client::new(rocket()).expect("valid rocket instance");
     let mut response = client.get("/api").dispatch();
     assert_eq!(response.status(), Status::Ok);
-    assert_eq!(response.body_string(), Some("Hello, world! from root".into()));
+    assert_eq!(response.body_string(), Some("It's homepage aka index".into()));
+}
+
+#[test]
+fn world() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let mut response = client.get("/api/world").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some("Hello, world!".into()));
 }
