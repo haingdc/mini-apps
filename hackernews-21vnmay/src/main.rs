812eddf01667322a62425728591e980dfa81815d
hai.nguyen <haingdc@gmail.com>
2021-06-16 02:00:37 +0700

    implement api articles

--- hackernews-21vnmay/src/main.rs
+++ hackernews-21vnmay/src/main.rs
@@ -2,7 +2,12 @@
 extern crate rss;
 
 #[macro_use] extern crate rocket;
-use rocket_contrib::templates::Template;
+use std::collections::HashMap;
+
+use rocket_contrib::{
+  templates::Template,
+  json::{Json},
+};
 
 extern crate serde;
 #[macro_use] extern crate serde_derive;
@@ -13,7 +18,7 @@ mod fetch;
 use fetch::*;
 
 fn main() {
-  let result = fetch_from("https://thefullsnack.com/rss.xml");
+  let _result = fetch_from("https://thefullsnack.com/rss.xml");
   // if result.is_ok() {
   //   let json_data = json!({ "items": result.unwrap() });
   //   println!("{}", json_data.to_string());
@@ -25,15 +30,29 @@ fn main() {
         // .extra("template_dir",  "web/templates")
         .unwrap();
   rocket::custom(cfg)
-    .mount("/", routes![index])
+    .mount("/", routes![index, json, get_articles])
     .attach(Template::fairing())
     .launch();
 }
 
-const RSS_URL: &str = "https://news.ycombinator.com/rss";
+const RSS_URL: &str = "https://thefullsnack.com/rss.xml";
 
 #[get("/")]
 fn index() -> Template {
   let news = fetch_from(RSS_URL).ok().expect("Could not read RSS");
   Template::render("index", &news)
+}
+
+#[get("/json")]
+fn json() -> Json<HashMap<String, String>> {
+  let mut my_map = HashMap::new();
+  my_map.insert(String::from("cheese"), String::from("gouda"));
+  my_map.insert(String::from("bread"), String::from("rye"));
+  return Json(my_map);
+}
+
+#[get("/articles")]
+fn get_articles() -> Json<Vec<RSSItem>> {
+  let news = fetch_from(RSS_URL).ok().expect("Could not read RSS");
+  return Json(news);
 }
