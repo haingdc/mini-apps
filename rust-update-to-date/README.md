48d555832ef41e3ff87e2bda0f9a986340e91d84
hai.nguyen <haingdc@gmail.com>
2021-06-19 19:07:00 +0700

    implement Drop trait

---
+++ rust-update-to-date/README.md
@@ -0,0 +1,3 @@
+# Serialize and Deserialize
+
+commit: 020990e17447e3197fe1641ff24212ef6605eae1
--- rust-update-to-date/src/main.rs
+++ rust-update-to-date/src/main.rs
@@ -1,36 +1,26 @@
 extern crate serde;
 
-use serde::{Serialize, Deserialize};
-
 fn main() {
-	let point1 = Point {x:1.0, y:2.0};
-	let point2 = Point {x:3.0, y:4.0};
-	let point1s = serde_json::to_string(&point1).unwrap();
-	let point2s = serde_json::to_string(&point2).unwrap();
-	println!("struct Point serializes into string {}", point1s);
-	println!("struct Point serializes into string {}", point2s);
+	println!("Begin outer scrope");
 
-	let length = ((point1.x - point2.x) * (point1.x - point2.x) + (point1.y - point2.y) * (point1.y - point2.y)).sqrt();
-	let valid = if length == 0.0 { false } else { true };
-	let line = Line { points: vec![point1, point2], valid: valid, length: length, desc: "a thin line".to_string() };
-	let lines = serde_json::to_string(&line).unwrap();
-	println!("struct Line serializes into string {}", lines);
+	{
+		println!("Begin inner scope");
+		let x = DropMe;
+		println!("x: {:?}", x);
+	}
 
-	let lined: Line = serde_json::from_str(&lines).unwrap();
-	assert_eq!(lined.desc, "a thin line");
-	assert_eq!(lined.points[1].x, 3.0);
+	println!("End outer scrope");
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Point {
-	x: f32,
-	y: f32
+#[derive(Debug)]
+struct DropMe;
+
+impl Drop for DropMe {
+
+	// drop is called before an instance is dropped
+	fn drop(&mut self) {
+		println!("Dropping!");
+	}
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Line {
-	points: Vec<Point>,
-	valid : bool,
-	length: f32,
-	desc  : String
-}
+
