dc850d4fe281490a83817802ef7f0e8c7222d9d8
hai.nguyen <haingdc@gmail.com>
2021-06-19 15:47:25 +0700

    serialization and deserialization demo 1

---
+++ rust-update-to-date/src/main.rs
@@ -0,0 +1,9 @@
+extern crate serde;
+
+fn main() {
+	let x = 5;
+	let xs = serde_json::to_string(&x).unwrap();
+	println!("i32 number {} serializes into string {}", x, xs);
+	let xd : i32 = serde_json::from_str(&xs).unwrap();
+	assert_eq!(x, xd);
+}
