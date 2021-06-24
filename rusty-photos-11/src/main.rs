4a0f3e060ff2049b93dda7a4545edb9551545c93
hai.nguyen <haingdc@gmail.com>
2021-06-25 02:54:07 +0700

    finding 1000th prime number

--- rusty-photos-11/src/main.rs
+++ rusty-photos-11/src/main.rs
@@ -1,13 +1,17 @@
-extern crate image;
+extern crate primal;
 
-use image::{GenericImageView};
+use primal::Sieve;
 
 fn main() {
-  let img = image::open("brian jackson.jpg").unwrap();
-  let (width, height) = img.dimensions();
-  println!("dimensions ({}, {})", width, height);
+  let sieve = Sieve::new(10_000);
+  let suspect = 5273;
+  println!("{} is prime: {}", suspect, sieve.is_prime(suspect));
+  let not_a_prime = 1024;
+  println!("{} is prime {}", not_a_prime, sieve.is_prime(not_a_prime));
 
-  println!("{:?}", img.color());
-
-  img.save("test.png").unwrap();
+  let n = 1000;
+  match sieve.primes_from(0).nth(n - 1) {
+    Some(number) => println!("{}th prime is {}", n, number),
+    None => println!("I don't know anything about {}th prime", n),
+  }
 }
