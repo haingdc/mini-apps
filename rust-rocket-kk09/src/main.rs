3e8885b6fc45d6129f2536db926b1e5dc3964ece
hai.nguyen <haingdc@gmail.com>
2021-07-04 17:51:53 +0700

    serve text file and dynamic path

--- rust-rocket-kk09/src/main.rs
+++ rust-rocket-kk09/src/main.rs
@@ -1,5 +1,17 @@
 #[macro_use] extern crate rocket;
 use rocket::tokio::time::{sleep, Duration};
+use rocket::tokio::task::spawn_blocking;
+use std::io;
+
+#[launch]
+fn rocket() -> _ {
+	rocket::build().mount("/hello", routes![
+		world,
+    delay,
+    serve_text_file,
+    greeting,
+	])
+}
 
 #[get("/delay/<seconds>")]
 async fn delay(seconds: u64) -> String {
@@ -12,10 +24,19 @@ fn world() -> &'static str {
 	"Hello, world!"
 }
 
-#[launch]
-fn rocket() -> _ {
-	rocket::build().mount("/hello", routes![
-		world,
-    delay,
-	])
+#[get("/serve_text_file")]
+async fn serve_text_file() -> io::Result<Vec<u8>> {
+  let vec = spawn_blocking(|| std::fs::read("data.txt")).await
+    .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, e))??;
+
+  Ok(vec)
+}
+
+#[get("/greeting/<name>/<age>/<cool>")]
+fn greeting(name: &str, age: u8, cool: bool) -> String {
+  if cool {
+    format!("You're a cool {} year old, {}!", age, name)
+  } else {
+    format!("{}, we need to talk about your coolness.", name)
+  }
 }
