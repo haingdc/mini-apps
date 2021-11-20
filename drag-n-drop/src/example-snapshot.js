4d9bd726f3b1339045ec034ab9892ccce1adaa66
hai.nguyen <haingdc@gmail.com>
2021-11-20 13:48:17 +0700

    add drag-n-drop example

---
+++ drag-n-drop/src/example-snapshot.js
@@ -0,0 +1,11 @@
+// Draggable
+const draggableSnapshot = {
+  isDragging: true,
+  draggingOver: 'column-1',  // OR null when we don't drag over any droppable
+};
+
+// Droppable
+const droppableSnapshot = {
+  isDraggingOver: true,
+  draggingOverWith: 'task-1', // OR null same thing but different context
+};
