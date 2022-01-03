a187ae349b4339cd7353b47c9d5a148dd90464cd
hai.nguyen <haingdc@gmail.com>
2022-01-03 11:04:22 +0700

    migrate to rocket 0.5.0-rc.1

--- hackernews-21vnmay/src/main.rs
+++ hackernews-21vnmay/src/main.rs
@@ -2,13 +2,13 @@
 extern crate rss;
 
 #[macro_use] extern crate rocket;
-use std::collections::HashMap;
+use std::{collections::HashMap, net::Ipv4Addr, io, path::{PathBuf, Path}};
 
-use rocket_contrib::{
-  templates::Template,
-  json::{Json},
-  serve::StaticFiles,
+use rocket::{
+  serde::json::Json,
+  tokio::task::spawn_blocking, fs::NamedFile,
 };
+use rocket_dyn_templates::Template;
 
 extern crate serde;
 #[macro_use] extern crate serde_derive;
@@ -18,31 +18,44 @@ extern crate serde;
 mod fetch;
 use fetch::*;
 
-fn main() {
-  let _result = fetch_from("https://thefullsnack.com/rss.xml");
+#[rocket::main]
+async fn main() {
+  // let _result = fetch_from("https://thefullsnack.com/rss.xml");
   // if result.is_ok() {
   //   let json_data = json!({ "items": result.unwrap() });
   //   println!("{}", json_data.to_string());
   // }
 
-  let cfg = rocket::config::Config::build(rocket::config::Environment::Development)
-        .address("127.0.0.1")
-        .port(3000)
-        // .extra("template_dir",  "web/templates")
-        .unwrap();
-  rocket::custom(cfg)
-  .mount("/", routes![index, json, get_articles])
-  .mount("/public", StaticFiles::from("public"))
+  let config = rocket::Config {
+    address: Ipv4Addr::new(127, 0, 0, 1).into(),
+    port: 8000,
+    ..rocket::Config::debug_default()
+  };
+
+  rocket::custom(&config)
+    .mount("/", routes![index, blocking_task, json, get_articles])
+    .mount("/public", routes![files])
     .attach(Template::fairing())
-    .launch();
+    .launch()
+    .await;
 }
 
+// const RSS_URL: &str = "https://blog.rust-lang.org/feed.xml";
+// const RSS_URL: &str = "https://news.ycombinator.com/rss";
 const RSS_URL: &str = "https://thefullsnack.com/rss.xml";
 
 #[get("/")]
 fn index() -> Template {
   let news = fetch_from(RSS_URL).ok().expect("Could not read RSS");
   Template::render("index", &news)
+}
+
+#[get("/blocking_task")]
+async fn blocking_task() -> std::io::Result<Vec<u8>> {
+  let vec = spawn_blocking(|| std::fs::read("public/data.txt")).await
+      .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, e))??;
+
+  Ok(vec)
 }
 
 #[get("/json")]
@@ -57,4 +70,9 @@ fn json() -> Json<HashMap<String, String>> {
 fn get_articles() -> Json<Vec<RSSItem>> {
   let news = fetch_from(RSS_URL).ok().expect("Could not read RSS");
   return Json(news);
+}
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+  NamedFile::open(Path::new("public/").join(file)).await.ok()
 }
